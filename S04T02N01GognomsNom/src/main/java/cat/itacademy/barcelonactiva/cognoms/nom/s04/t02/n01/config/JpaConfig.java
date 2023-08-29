40312baf65d08d87246b9b514f70a3cd58c0dd1f
Darko <132366394+darkomoranini@users.noreply.github.com>
2023-08-29 10:56:18 +0200

    Delete S04T02N01GognomsNom directory

--- S04T02N01GognomsNom/src/main/java/cat/itacademy/barcelonactiva/cognoms/nom/s04/t02/n01/config/JpaConfig.java
+++
@@ -1,9 +0,0 @@
-package cat.itacademy.barcelonactiva.cognoms.nom.s04.t02.n01.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-@Configuration
-@EnableJpaRepositories(basePackages = "cat.itacademy.barcelonactiva.cognoms.nom.s04.t02.n01.model.repository")
-public class JpaConfig {
-}
